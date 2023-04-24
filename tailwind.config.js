1f65159d975e86f0a28eed31467ddbaed9d0cc99
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2023-04-24 10:57:15 +0200

    add new font
    nunito

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        nunito:" 'ninoto',sans-serif "
+      }
+    },
   },
   plugins: [],
 }
